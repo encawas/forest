bc53691fb6833b7db93ba0df5fe83e7765b7833d
encawas <3517417137@qq.com>
2026-07-09 19:29:54 +0800

    Add C5 touch coordinate hit-test API

--- firmware_modules/c5/touch_actions.h
+++ firmware_modules/c5/touch_actions.h
@@ -14,11 +14,27 @@ typedef enum {
 } c5_touch_zone_t;
 
 typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} c5_touch_rect_t;
+
+typedef struct {
+    c5_touch_rect_t start;
+    c5_touch_rect_t break_button;
+    c5_touch_rect_t end;
+} c5_touch_layout_t;
+
+typedef struct {
     int has_action;
     echoforest_control_action_t action;
 } c5_touch_action_result_t;
 
+c5_touch_layout_t c5_default_touch_layout(int screen_width, int screen_height);
+c5_touch_zone_t c5_touch_point_to_zone(int x, int y, const c5_touch_layout_t *layout);
 c5_touch_action_result_t c5_touch_zone_to_action(c5_touch_zone_t zone);
+c5_touch_action_result_t c5_touch_point_to_action(int x, int y, const c5_touch_layout_t *layout);
 
 #ifdef __cplusplus
 }
