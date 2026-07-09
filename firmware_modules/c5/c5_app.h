fa51657c3e8cd9be26c42ceb3787aaebeb8fa721
encawas <3517417137@qq.com>
2026-07-09 19:30:20 +0800

    Expose C5 touch point handler

--- firmware_modules/c5/c5_app.h
+++ firmware_modules/c5/c5_app.h
@@ -15,6 +15,7 @@ void c5_app_init(
 );
 
 int c5_app_handle_touch(c5_touch_zone_t zone, const char *goal);
+int c5_app_handle_touch_point(int x, int y, const c5_touch_layout_t *layout, const char *goal);
 int c5_app_poll_backend(c5_screen_frame_t *out_frame, c5_speaker_prompt_t *out_prompt);
 const echoforest_state_snapshot_t *c5_app_cached_state(void);
 
