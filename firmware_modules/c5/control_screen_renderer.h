b6723128b815cd55d4f1275142eef382fceabc70
encawas <3517417137@qq.com>
2026-07-09 17:24:44 +0800

    Add C5 control screen renderer header

---
+++ firmware_modules/c5/control_screen_renderer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../common/echoforest_protocol.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define C5_SCREEN_LINE_COUNT 6
+#define C5_SCREEN_LINE_LEN 64
+
+typedef struct {
+    char lines[C5_SCREEN_LINE_COUNT][C5_SCREEN_LINE_LEN];
+} c5_screen_frame_t;
+
+void c5_render_control_screen(
+    const echoforest_state_snapshot_t *state,
+    c5_screen_frame_t *out_frame
+);
+
+#ifdef __cplusplus
+}
+#endif
