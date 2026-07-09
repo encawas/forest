04a956ea4f1735a4b84cf3e5d2a3975e9d6b7426
encawas <3517417137@qq.com>
2026-07-09 17:24:21 +0800

    Add firmware cloud client interface

---
+++ firmware_modules/common/cloud_client.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "echoforest_protocol.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    const char *backend_base_url;
+    const char *device_id;
+    const char *device_role;
+} echoforest_cloud_config_t;
+
+typedef struct {
+    int (*post_control)(echoforest_control_action_t action, const char *source, const char *goal);
+    int (*post_heartbeat)(const char *device_id, const char *role, const char *source);
+    int (*get_state)(echoforest_state_snapshot_t *out_state);
+} echoforest_cloud_transport_t;
+
+void echoforest_cloud_client_init(
+    const echoforest_cloud_config_t *config,
+    const echoforest_cloud_transport_t *transport
+);
+
+int echoforest_cloud_post_control(echoforest_control_action_t action, const char *goal);
+int echoforest_cloud_post_heartbeat(void);
+int echoforest_cloud_get_state(echoforest_state_snapshot_t *out_state);
+
+#ifdef __cplusplus
+}
+#endif
