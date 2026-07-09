6cbd5db5b136cbaadbc911466870ac94f66f442b
encawas <3517417137@qq.com>
2026-07-09 17:47:01 +0800

    Add environment upload to firmware cloud client interface

--- firmware_modules/common/cloud_client.h
+++ firmware_modules/common/cloud_client.h
@@ -14,6 +14,7 @@ typedef struct {
 
 typedef struct {
     int (*post_control)(echoforest_control_action_t action, const char *source, const char *goal);
+    int (*post_environment)(const char *device_id, const char *source, const echoforest_environment_t *environment);
     int (*post_heartbeat)(const char *device_id, const char *role, const char *source);
     int (*get_state)(echoforest_state_snapshot_t *out_state);
 } echoforest_cloud_transport_t;
@@ -24,6 +25,7 @@ void echoforest_cloud_client_init(
 );
 
 int echoforest_cloud_post_control(echoforest_control_action_t action, const char *goal);
+int echoforest_cloud_post_environment(const echoforest_environment_t *environment);
 int echoforest_cloud_post_heartbeat(void);
 int echoforest_cloud_get_state(echoforest_state_snapshot_t *out_state);
 
